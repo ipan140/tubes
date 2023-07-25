52e766f886cba6a3c7941e6037e6c742fdd3463b
ipan140 <ipanrossi2@email.com>
2023-07-25 14:14:59 +0700

    ivano upload

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\SesiController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', [SesiController::class, 'index']);
+    Route::get('/admin', [AdminController::class, 'index']);
+    Route::post('/', [SesiController::class, 'login']);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
