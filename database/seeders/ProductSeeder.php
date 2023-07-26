e03ca29f5356b5aa852e19fa46025d319504843c
alytamam <alytmaam@gmail.com>
2023-07-26 17:31:26 +0700

    perubahan produk seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,5 +15,17 @@ class ProductSeeder extends Seeder
             'product_photo_filename' => '',
             'product_price' => 45000,
         ]);
+
+        Product::create([
+            'product_name' => 'ipan',
+            'product_photo_filename' => '',
+            'product_price' => 50000,
+        ]);
+
+        Product::create([
+            'product_name' => 'husni',
+            'product_photo_filename' => '',
+            'product_price' => 60000,
+        ]);
     }
 }
