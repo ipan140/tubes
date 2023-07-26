a603abfe3271c4be3ce05662a21ae0d97acfd0a9
alytamam <alytmaam@gmail.com>
2023-07-26 16:10:24 +0700

    percobaan membuat cart

--- app/Http/Controllers/HalProdukController.php
+++ app/Http/Controllers/HalProdukController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Gloudemans\Shoppingcart\Facades\Cart;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HalProdukController extends Controller
@@ -9,6 +10,30 @@ class HalProdukController extends Controller
     function index()
     {
     $pageTitle = 'HalamanProduk';
-    return view('HalamanProduk', ['pageTitle' => $pageTitle,]);
+    $products = Product::all();
+    return view('HalamanProduk', ['pageTitle' => $pageTitle,'products' => $products]);
+    }
+
+    public function addToCart(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        // Use the username if logged in, 'guest' if not
+        $identifier = auth()->user()->name;
+
+        Cart::instance($identifier)->add([
+            'id' => $product->id,
+            'name' => $product->product_album,
+            'qty' => 1,
+            'price' => $product->product_price,
+            'options' => [
+                'artist' => $product->product_artist,
+                // Add more options as needed
+            ]
+        ]);
+
+        return redirect()->back()->with('success', 'Item added to cart.');
     }
 }
+
+
