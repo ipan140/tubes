88b5fd73720632e55b9d7698bf9a428654710333
Muhammad Husni Mubarrok <mubarrokhusni12@gmail.com>
2023-08-01 17:04:16 +0700

    push

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,7 +6,7 @@
         <hr>
         <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3 border">
             <div class="bi-person-circle me-3 fs-1"></div>
-            <h4 class="mb-0">Well done! this is {{ $pageTitle }}.</h4>
+            <h4 class="mb-0">Well done! this is {{ $pageTitle }}.jjjjjkkkkm</h4>
         </div>
 </div>
     </div>
