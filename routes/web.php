17e5257062c1ee6d06f7cbe653c05ba0bcc2605d
Muhammad Husni Mubarrok <mubarrokhusni12@gmail.com>
2023-08-02 10:55:39 +0700

    penambahan pembayaran,inheritance,merapikan route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\KasirController;
 use App\Http\Controllers\DatabarangController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -22,7 +21,7 @@ use App\Http\Controllers\ProfileController;
 
 Auth::routes();
 
-Route::get('profile', ProfileController::class)->name('profile');
+
 // Route::get('/HalamanProduk', [App\Http\Controllers\HalProdukController::class, 'index'])->name('HalamanProduk');
 // Route::get('/HalamanAwal', [App\Http\Controllers\HalAwalController::class, 'index'])->name('HalamanAwal');
 Route::get('/HalamanProduk/{id}', [App\Http\Controllers\HomeController::class, 'show'])->name('HalamanProduk');
@@ -35,35 +34,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// route untuk dashboard admin
-// Route::get('/dashboard', function () {
-//     return view('dashboard.index');
-// });
-
-// Route::get('/pembayaran', function () {
-//     return view('pembayaran');
-// });
-
-// Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
-// Route::resource('Databarang', DatabarangController::class);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Rest of your code...
+
 
 Route::middleware(['auth', 'User'])->group(function () {
-    // Route::get('/dashboarduser', [KasirController::class, 'index'])->name('homekasir');
     Route::get('profile', ProfileController::class)->name('profile');
-    // Route::get('/HalamanProduk', [App\Http\Controllers\HalProdukController::class, 'index'])->name('HalamanProduk');
-    // Rest of your routes...
+    Route::get('/pembayaran', [App\Http\Controllers\PembayaranController::class, 'index'])->name('pembayaran');
+
 });
 
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/dashboardadmin', [DatabarangController::class, 'index'])->name('dashboardadmin');
     Route::resource('Databarang', DatabarangController::class);
-    // Route::put('/Databarang/{id}', [DatabarangController::class, 'update'])->name('Databarang.update');
-    // Route::get('/Databarang/{id}/edit', [DatabarangController::class, 'edit'])->name('Databarang.edit');
     Route::delete('/Databarang/{id}', 'DatabarangController@destroy')->name('dashboard.destroy');
-    // Route::resource('admin', AdminController::class);
-    // Rest of your routes...
+
 });
